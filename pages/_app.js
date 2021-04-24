1e9a4a19bda3607c926c84183c7295550f1ca2fe
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-04-24 22:38:45 +0700

    New Malanghub

--- pages/_app.js
+++ pages/_app.js
@@ -12,33 +12,6 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,uc-fitscreen=yes"
         />
-        <meta name="title" content="Malanghub" />
-        <meta
-          name="description"
-          content="Situs yang menyediakan informasi sekitar Malang Raya!"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.malanghub.com/" />
-        <meta property="og:title" content="Malanghub" />
-        <meta
-          property="og:description"
-          content="Situs yang menyediakan informasi sekitar Malang Raya!"
-        />
-        <meta
-          property="og:image"
-          content="https://www.malanghub.com/malanghub-meta.png"
-        />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.malanghub.com/" />
-        <meta property="twitter:title" content="Malanghub" />
-        <meta
-          property="twitter:description"
-          content="Situs yang menyediakan informasi sekitar Malang Raya!"
-        />
-        <meta
-          property="twitter:image"
-          content="https://www.malanghub.com/malanghub-meta.png"
-        />
       </Head>
 
       <Header />
