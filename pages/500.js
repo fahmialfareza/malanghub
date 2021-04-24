03872b748fa12614d1a7e6f4110eb11b24dcc84f
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-04-24 23:24:40 +0700

    A Little Fix Bugs

--- pages/500.js
+++ pages/500.js
@@ -2,7 +2,7 @@ export default function Custom500() {
   return (
     <div
       className="container"
-      style={{ width: "100%", height: "80vh", display: "flex" }}
+      style={{ width: "100%", height: "50vh", display: "flex" }}
     >
       <div className="m-auto">
         <h1>404 - Kesalahan Server</h1>
