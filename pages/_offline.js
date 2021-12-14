333c0969a3148e28028b45b2d52465a3a6ad997b
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-12-14 22:16:32 +0700

    Add offline page

---
+++ pages/_offline.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import { wrapper } from '../redux/store';
+import Header from '../components/layouts/Header';
+import Footer from '../components/layouts/Footer';
+import Alert from '../components/layouts/Alert';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Malanghub</title>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+      </Head>
+
+      <Header />
+
+      <Alert />
+
+      <div>
+        <h1>Kamu sedang offline</h1>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default wrapper.withRedux(MyApp);
