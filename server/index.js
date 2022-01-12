5fdd84abdc9f9ab0efcee341ace8ee86771258f1
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-12 10:50:59 +0700

    Edit API_ADDRESS

--- server/index.js
+++ server/index.js
@@ -1,35 +1,37 @@
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
-const path = require("path");
-const fs = require("fs");
-const express = require("express");
-const next = require("next");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
-const hpp = require("hpp");
-const helmet = require("helmet");
-const cors = require("cors");
-const fileUpload = require("express-fileupload");
-const morgan = require("morgan");
-const { join } = require("path");
-const { parse } = require("url");
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const next = require('next');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+const { join } = require('path');
+const { parse } = require('url');
 
 const PORT = process.env.PORT || 3000;
-const app = next({ dev: process.env.NODE_ENV !== "production" });
+process.env.API_ADDRESS = 'http://localhost:' + process.env.PORT;
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   // Import error handler
-  const errorHandler = require("./middlewares/errorHandler");
+  const errorHandler = require('./middlewares/errorHandler');
 
   // Import routes
-  const usersRoute = require("./routes/usersRoute");
-  const newsCategoriesRoute = require("./routes/newsCategoriesRoute");
-  const newsTagsRoute = require("./routes/newsTagsRoute");
-  const newsDraftsRoute = require("./routes/newsDraftsRoute");
-  const newsRoute = require("./routes/newsRoute");
-  const newsCommentsRoute = require("./routes/newsCommentsRoute");
-  const imageUploadRoute = require("./routes/imageUploadRoute");
+  const usersRoute = require('./routes/usersRoute');
+  const newsCategoriesRoute = require('./routes/newsCategoriesRoute');
+  const newsTagsRoute = require('./routes/newsTagsRoute');
+  const newsDraftsRoute = require('./routes/newsDraftsRoute');
+  const newsRoute = require('./routes/newsRoute');
+  const newsCommentsRoute = require('./routes/newsCommentsRoute');
+  const imageUploadRoute = require('./routes/imageUploadRoute');
 
   const server = express();
 
@@ -68,37 +70,37 @@ app.prepare().then(() => {
     })
   );
 
-  if (process.env.NODE_ENV === "development") {
-    server.use(morgan("dev"));
+  if (process.env.NODE_ENV === 'development') {
+    server.use(morgan('dev'));
   } else {
     // create a write stream (in serverend mode)
     let accessLogStream = fs.createWriteStream(
-      path.join(__dirname, "access.log"),
+      path.join(__dirname, 'access.log'),
       {
-        flags: "a",
+        flags: 'a',
       }
     );
 
     // setup the logger
-    server.use(morgan("combined", { stream: accessLogStream }));
+    server.use(morgan('combined', { stream: accessLogStream }));
   }
 
   // Use fileUpload
   server.use(fileUpload());
 
   // Use static files
-  server.use(express.static("public"));
+  server.use(express.static('public'));
 
   // Routes
-  server.use("/api/users", usersRoute);
-  server.use("/api/newsCategories", newsCategoriesRoute);
-  server.use("/api/newsTags", newsTagsRoute);
-  server.use("/api/newsDrafts", newsDraftsRoute);
-  server.use("/api/news", newsRoute);
-  server.use("/api/newsComments", newsCommentsRoute);
-  server.use("/api/upload", imageUploadRoute);
+  server.use('/api/users', usersRoute);
+  server.use('/api/newsCategories', newsCategoriesRoute);
+  server.use('/api/newsTags', newsTagsRoute);
+  server.use('/api/newsDrafts', newsDraftsRoute);
+  server.use('/api/news', newsRoute);
+  server.use('/api/newsComments', newsCommentsRoute);
+  server.use('/api/upload', imageUploadRoute);
 
-  server.all("*", (req, res) => {
+  server.all('*', (req, res) => {
     return handle(req, res);
   });
 
