7b47e40ecc779f70a75e1d31cfdd9ecbb490da88
Fahmi Alfareza <fahmialfareza@icloud.com>
2024-08-22 17:06:07 +0700

    revamp: migrate to DO Kubernetes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+if (process.env.NODE_ENV === "production") {
+  require("dotenv").config();
+} else {
+  require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+}
+
 require("newrelic");
 const path = require("path");
 const fs = require("fs");
