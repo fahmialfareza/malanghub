de4368d45328bf6a738d6ae2e9f877b2b444e18f
Fahmi Alfareza <signer_aero0l@icloud.com>
2025-05-10 15:38:22 +0700

    migrate: to netlify

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,55 +1,35 @@
 const withImages = require("next-images");
-
+const { withNetlify } = require("@netlify/next");
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   cacheOnFrontEndNav: true,
   disable: process.env.NODE_ENV === "development",
 });
-
-module.exports = withPWA(
-  withImages({
-    images: {
-      disableStaticImages: true,
-    },
-    compress: true,
-  })
-);
-
-// Injected content via Sentry wizard below
-
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withSentryConfig(
-  module.exports,
+const baseConfig = withImages({
+  images: {
+    disableStaticImages: true,
+  },
+  compress: true,
+});
+
+const sentryWrappedConfig = withSentryConfig(
+  baseConfig,
   {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
     silent: true,
-
     org: process.env.SENTRY_ORG,
     project: process.env.SENTRY_PROJECT,
-
     authToken: process.env.SENTRY_AUTH_TOKEN,
   },
   {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
   }
 );
+
+// Final export
+module.exports = withNetlify(withPWA(sentryWrappedConfig));
